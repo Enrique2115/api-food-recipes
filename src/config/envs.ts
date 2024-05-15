4e51db3be20d274fbbf030edcf20dbf6728a5230
Luis Enrique <lmorochofebres@gmail.com>
2024-05-14 22:03:30 -0500

    style: :art: changes in the code format

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -7,6 +7,11 @@ interface EnvVars {
   PORT: number;
   HOST: string;
   NODE_ENV: string;
+
+  DB_URL: string;
+  API_KEY: string;
+  API_SECRET: string;
+  API_NAME: string;
 }
 
 const envsSchema = joi
@@ -14,6 +19,10 @@ const envsSchema = joi
     PORT: joi.number().required(),
     HOST: joi.string().required(),
     NODE_ENV: joi.string().required(),
+    DB_URL: joi.string().required(),
+    API_KEY: joi.string().required(),
+    API_SECRET: joi.string().required(),
+    API_NAME: joi.string().required(),
   })
   .unknown(true);
 
@@ -29,4 +38,9 @@ export const envs = {
   PORT: envVars.PORT,
   HOST: envVars.HOST,
   NODE_ENV: envVars.NODE_ENV,
+
+  DB_URL: envVars.DB_URL,
+  API_KEY: envVars.API_KEY,
+  API_SECRET: envVars.API_SECRET,
+  API_NAME: envVars.API_NAME,
 };
